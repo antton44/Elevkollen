fe19530caa4f7ba88f77ebc8b7b3a708ba1f7668
Bastian <bastian@lemstrom.se>
2017-11-26 12:15:15 +0100

    Update OldDeletedDataState.java

--- data/persistanceFacade/state/OldDeletedDataState.java
+++ data/persistanceFacade/state/OldDeletedDataState.java
@@ -26,4 +26,7 @@ public class OldDeletedDataState implements IDataState{
 	public void delete(DataTransferObject dto) {
 		dto.setState(this);
 	}
+	public String toString(){
+		return "Old Deleted Data State";
+	}
 }
