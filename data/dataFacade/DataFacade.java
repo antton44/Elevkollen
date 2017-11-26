a3c886c838266def092c213e410bea5b81bab8ad
antton44 <antton44@gmail.com>
2017-11-26 12:59:36 +0100

    Update DataFacade.java

--- data/dataFacade/DataFacade.java
+++ data/dataFacade/DataFacade.java
@@ -2,10 +2,13 @@ package data.dataFacade;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import data.persistanceFacade.factory.*;
 import data.dataTransferObject.*;
 import data.persistanceFacade.broker.*;
 import domain.entities.Absence;
+import domain.entities.Parent;
+import domain.entities.Semester;
 import domain.entities.Student;
 import domain.entities.Teacher;
 
@@ -14,9 +17,14 @@ public class DataFacade {
 	private StudentDTO sDTO;
 	private TeacherBroker tb;
 	private TeacherDTO tDTO;
+	private ParentBroker pb;
+	private ParentDTO pDTO;
+	private SemesterBroker semb;
+	private SemesterDTO semDTO;
 	private Object obj;
 	private EntityFactory ef;
 	
+	//Student
 	public Object getStudent()
 	{
 		try {
@@ -75,6 +83,8 @@ public class DataFacade {
 		return sDTO;
 	}
 	
+	
+	//Teacher
 	public Object getTeacher()
 	{
 		try {
@@ -82,13 +92,15 @@ public class DataFacade {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		tDTO = new TeacherDTO(obj.toString());
+		String s1 = obj.toString();
+		String[] splitter1 = s1.split("---|\\n");
+		tDTO = new TeacherDTO(splitter1[0], splitter1[1],splitter1[2]);
 		return tDTO;
 	}
 	
 	public void addTeacher(Teacher teacher)
 	{
-		TeacherDTO dto = new TeacherDTO(teacher.toString());
+		TeacherDTO dto = new TeacherDTO(teacher.getPersonnummer(), teacher.getName(), teacher.getEmail());
 		try {
 			tb.insertStorage(dto);
 		} catch (SQLException e) {
@@ -114,7 +126,7 @@ public class DataFacade {
 		}
 	}
 	
-	public Object findSTeacher(Teacher teacher)
+	public Object findTeacher(Teacher teacher)
 	{
 		try {
 			obj = tb.findInStorage(teacher);
@@ -123,7 +135,124 @@ public class DataFacade {
 		}
 		String s1 = obj.toString();
 		String[] splitter1 = s1.split("---|\\n");
-		tDTO = new TeacherDTO(teacher.toString());
+		tDTO = new TeacherDTO(splitter1[0], splitter1[1],splitter1[2]);
 		return tDTO;
 	}
+	
+	
+	//Parent
+	public Object getParent()
+	{
+		try {
+			 obj = pb.getFromStorage(new Object());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		String s1 = obj.toString();
+		String[] splitter1 = s1.split("---|\\n");
+		pDTO = new ParentDTO(splitter1[0], splitter1[1],splitter1[2]);
+		
+		return pDTO;
+	}
+	
+	public void addParent(Parent parent)
+	{
+		ParentDTO dto = new ParentDTO(parent.getPersonnummer(), parent.getName(), parent.getEmail());
+		try {
+			pb.insertStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateParent(Parent parent)
+	{
+		ParentDTO dto = new ParentDTO(parent.getPersonnummer(), parent.getName(), parent.getEmail());
+		try {
+			pb.updateStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteStudent(Parent parent)
+	{
+		ParentDTO dto = new ParentDTO(parent.getPersonnummer(), parent.getName(), parent.getEmail());
+		try {
+			pb.deleteStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Object findStudent(Parent parent)
+	{
+		ParentDTO dto = new ParentDTO(parent.getPersonnummer(), parent.getName(), parent.getEmail());
+		try {
+			obj = pb.findInStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		String s1 = obj.toString();
+		String[] splitter1 = s1.split("---|\\n");
+		pDTO = new ParentDTO(splitter1[0], splitter1[1],splitter1[2]);
+		return pDTO;
+	}
+	
+	//Semester
+	public Object getSemester()
+	{
+		try {
+			 obj = semb.getFromStorage(new Object());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		semDTO = new SemesterDTO(obj.toString());
+		return semDTO;
+	}
+	
+	public void addSemester(Semester semester)
+	{
+		SemesterDTO dto = new SemesterDTO(semester.getSemesterID());
+		try {
+			semb.insertStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateSemester(Semester semester)
+	{
+		SemesterDTO dto = new SemesterDTO(semester.getSemesterID());
+		try {
+			semb.updateStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteSemester(Semester semester)
+	{
+		SemesterDTO dto = new SemesterDTO(semester.getSemesterID());
+		try {
+			semb.deleteStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Object findSemester(Semester semester)
+	{
+		SemesterDTO dto = new SemesterDTO(semester.getSemesterID());
+		try {
+			obj = semb.findInStorage(dto);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		semDTO = new SemesterDTO(obj.toString());
+		return semDTO;
+	}
+	
+	//
 }
+
