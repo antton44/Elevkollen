50e43f8db95855bce2ab11932d7cf67634495495
rasje314 <rasje314@student.liu.se>
2018-01-19 15:02:56 +0100

    Updated Start to work with changes to login gui (states)

--- main/Start.java
+++ main/Start.java
@@ -10,9 +10,9 @@ public class Start {
 	public static void main(String[] args) {
 		Login login = new Login();
 		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		login.setSize(360,600);
+		login.setSize(360, 600);
 		login.setLayout(new BorderLayout());
+		login.init();
 		login.setVisible(true);
-		login.addComponents();
 	}
 }
